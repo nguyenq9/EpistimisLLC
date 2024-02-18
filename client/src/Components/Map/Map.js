089c4ab2b6f6f84cf5a4ede3ce0c3e8a85684b68
nguyenq9 <nguyenq9@wwu.edu>
2024-02-18 00:17:50 -0800

    added db information to modal

--- client/src/Components/Map/Map.js
+++ client/src/Components/Map/Map.js
@@ -3,6 +3,7 @@ import "./Map.css";
 import { VectorMap } from "@react-jvectormap/core";
 import { usLcc } from "@react-jvectormap/unitedstates";
 import { worldMill } from "@react-jvectormap/world";
+import regionNames from "./regionNames.json";
 
 import Modal from "../Modal/Modal";
 
@@ -11,309 +12,11 @@ var initialArray = [];
 const Map = ({ isUS }) => {
   const [currCode, setCode] = useState("");
   const [showModal, setShowModal] = useState(false);
-  const [regionNames, setRegionNames] = useState({
-    "US-AL": "Alabama",
-    "US-AK": "Alaska",
-    "US-AZ": "Arizona",
-    "US-AR": "Arkansas",
-    "US-CA": "California",
-    "US-CO": "Colorado",
-    "US-CT": "Connecticut",
-    "US-DE": "Delaware",
-    "US-FL": "Florida",
-    "US-GA": "Georgia",
-    "US-HI": "Hawaii",
-    "US-ID": "Idaho",
-    "US-IL": "Illinois",
-    "US-IN": "Indiana",
-    "US-IA": "Iowa",
-    "US-KS": "Kansas",
-    "US-KY": "Kentucky",
-    "US-LA": "Louisiana",
-    "US-ME": "Maine",
-    "US-MD": "Maryland",
-    "US-MA": "Massachusetts",
-    "US-MI": "Michigan",
-    "US-MN": "Minnesota",
-    "US-MS": "Mississippi",
-    "US-MO": "Missouri",
-    "US-MT": "Montana",
-    "US-NE": "Nebraska",
-    "US-NV": "Nevada",
-    "US-NH": "New Hampshire",
-    "US-NJ": "New Jersey",
-    "US-NM": "New Mexico",
-    "US-NY": "New York",
-    "US-NC": "North Carolina",
-    "US-ND": "North Dakota",
-    "US-OH": "Ohio",
-    "US-OK": "Oklahoma",
-    "US-OR": "Oregon",
-    "US-PA": "Pennsylvania",
-    "US-RI": "Rhode Island",
-    "US-SC": "South Carolina",
-    "US-SD": "South Dakota",
-    "US-TN": "Tennessee",
-    "US-TX": "Texas",
-    "US-UT": "Utah",
-    "US-VT": "Vermont",
-    "US-VA": "Virginia",
-    "US-WA": "Washington",
-    "US-WV": "West Virginia",
-    "US-WI": "Wisconsin",
-    "US-WY": "Wyoming",
-    AF: "Afghanistan",
-    AL: "Albania",
-    DZ: "Algeria",
-    AS: "American Samoa",
-    AD: "Andorra",
-    AO: "Angola",
-    AI: "Anguilla",
-    AQ: "Antarctica",
-    AG: "Antigua and Barbuda",
-    AR: "Argentina",
-    AM: "Armenia",
-    AW: "Aruba",
-    AU: "Australia",
-    AT: "Austria",
-    AZ: "Azerbaijan",
-    BS: "Bahamas",
-    BH: "Bahrain",
-    BD: "Bangladesh",
-    BB: "Barbados",
-    BY: "Belarus",
-    BE: "Belgium",
-    BZ: "Belize",
-    BJ: "Benin",
-    BM: "Bermuda",
-    BT: "Bhutan",
-    BO: "Bolivia",
-    BQ: "Bonaire, Sint Eustatius and Saba",
-    BA: "Bosnia and Herzegovina",
-    BW: "Botswana",
-    BV: "Bouvet Island",
-    BR: "Brazil",
-    IO: "British Indian Ocean Territory",
-    BN: "Brunei Darussalam",
-    BG: "Bulgaria",
-    BF: "Burkina Faso",
-    BI: "Burundi",
-    CV: "Cabo Verde",
-    KH: "Cambodia",
-    CM: "Cameroon",
-    CA: "Canada",
-    KY: "Cayman Islands",
-    CF: "Central African Republic",
-    TD: "Chad",
-    CL: "Chile",
-    CN: "China",
-    CX: "Christmas Island",
-    CC: "Cocos (Keeling) Islands",
-    CO: "Colombia",
-    KM: "Comoros",
-    CG: "Congo",
-    CD: "Congo, Democratic Republic of the",
-    CK: "Cook Islands",
-    CR: "Costa Rica",
-    HR: "Croatia",
-    CU: "Cuba",
-    CW: "Curaçao",
-    CY: "Cyprus",
-    CZ: "Czechia",
-    CI: "Côte d'Ivoire",
-    DK: "Denmark",
-    DJ: "Djibouti",
-    DM: "Dominica",
-    DO: "Dominican Republic",
-    EC: "Ecuador",
-    EG: "Egypt",
-    SV: "El Salvador",
-    GQ: "Equatorial Guinea",
-    ER: "Eritrea",
-    EE: "Estonia",
-    SZ: "Eswatini",
-    ET: "Ethiopia",
-    FK: "Falkland Islands",
-    FO: "Faroe Islands",
-    FJ: "Fiji",
-    FI: "Finland",
-    FR: "France",
-    GF: "French Guiana",
-    PF: "French Polynesia",
-    TF: "French Southern Territories",
-    GA: "Gabon",
-    GM: "Gambia",
-    GE: "Georgia",
-    DE: "Germany",
-    GH: "Ghana",
-    GI: "Gibraltar",
-    GR: "Greece",
-    GL: "Greenland",
-    GD: "Grenada",
-    GP: "Guadeloupe",
-    GU: "Guam",
-    GT: "Guatemala",
-    GG: "Guernsey",
-    GN: "Guinea",
-    GW: "Guinea-Bissau",
-    GY: "Guyana",
-    HT: "Haiti",
-    HM: "Heard Island and McDonald Islands",
-    VA: "Holy See",
-    HN: "Honduras",
-    HK: "Hong Kong",
-    HU: "Hungary",
-    IS: "Iceland",
-    IN: "India",
-    ID: "Indonesia",
-    IR: "Iran",
-    IQ: "Iraq",
-    IE: "Ireland",
-    IM: "Isle of Man",
-    IL: "Israel",
-    IT: "Italy",
-    JM: "Jamaica",
-    JP: "Japan",
-    JE: "Jersey",
-    JO: "Jordan",
-    KZ: "Kazakhstan",
-    KE: "Kenya",
-    KI: "Kiribati",
-    KP: "Korea (the Democratic People's Republic of)",
-    KR: "Korea (the Republic of)",
-    KW: "Kuwait",
-    KG: "Kyrgyzstan",
-    LA: "Lao People's Democratic Republic",
-    LV: "Latvia",
-    LB: "Lebanon",
-    LS: "Lesotho",
-    LR: "Liberia",
-    LY: "Libya",
-    LI: "Liechtenstein",
-    LT: "Lithuania",
-    LU: "Luxembourg",
-    MO: "Macao",
-    MG: "Madagascar",
-    MW: "Malawi",
-    MY: "Malaysia",
-    MV: "Maldives",
-    ML: "Mali",
-    MT: "Malta",
-    MH: "Marshall Islands",
-    MQ: "Martinique",
-    MR: "Mauritania",
-    MU: "Mauritius",
-    YT: "Mayotte",
-    MX: "Mexico",
-    FM: "Micronesia",
-    MD: "Moldova",
-    MC: "Monaco",
-    MN: "Mongolia",
-    ME: "Montenegro",
-    MS: "Montserrat",
-    MA: "Morocco",
-    MZ: "Mozambique",
-    MM: "Myanmar",
-    NA: "Namibia",
-    NR: "Nauru",
-    NP: "Nepal",
-    NL: "Netherlands",
-    NC: "New Caledonia",
-    NZ: "New Zealand",
-    NI: "Nicaragua",
-    NE: "Niger",
-    NG: "Nigeria",
-    NU: "Niue",
-    NF: "Norfolk Island",
-    MK: "North Macedonia",
-    MP: "Northern Mariana Islands",
-    NO: "Norway",
-    OM: "Oman",
-    PK: "Pakistan",
-    PW: "Palau",
-    PS: "Palestine, State of",
-    PA: "Panama",
-    PG: "Papua New Guinea",
-    PY: "Paraguay",
-    PE: "Peru",
-    PH: "Philippines",
-    PN: "Pitcairn",
-    PL: "Poland",
-    PT: "Portugal",
-    PR: "Puerto Rico",
-    QA: "Qatar",
-    RO: "Romania",
-    RU: "Russian Federation",
-    RW: "Rwanda",
-    RE: "Réunion",
-    BL: "Saint Barthélemy",
-    SH: "Saint Helena, Ascension and Tristan da Cunha",
-    KN: "Saint Kitts and Nevis",
-    LC: "Saint Lucia",
-    MF: "Saint Martin (French part)",
-    PM: "Saint Pierre and Miquelon",
-    VC: "Saint Vincent and the Grenadines",
-    WS: "Samoa",
-    SM: "San Marino",
-    ST: "Sao Tome and Principe",
-    SA: "Saudi Arabia",
-    SN: "Senegal",
-    RS: "Serbia",
-    SC: "Seychelles",
-    SL: "Sierra Leone",
-    SG: "Singapore",
-    SX: "Sint Maarten (Dutch part)",
-    SK: "Slovakia",
-    SI: "Slovenia",
-    SB: "Solomon Islands",
-    SO: "Somalia",
-    ZA: "South Africa",
-    GS: "South Georgia and the South Sandwich Islands",
-    SS: "South Sudan",
-    ES: "Spain",
-    LK: "Sri Lanka",
-    SD: "Sudan",
-    SR: "Suriname",
-    SJ: "Svalbard and Jan Mayen",
-    SE: "Sweden",
-    CH: "Switzerland",
-    SY: "Syrian Arab Republic",
-    TW: "Taiwan",
-    TJ: "Tajikistan",
-    TZ: "Tanzania",
-    TH: "Thailand",
-    TL: "Timor-Leste",
-    TG: "Togo",
-    TK: "Tokelau",
-    TO: "Tonga",
-    TT: "Trinidad and Tobago",
-    TN: "Tunisia",
-    TR: "Turkey",
-    TM: "Turkmenistan",
-    TC: "Turks and Caicos Islands",
-    TV: "Tuvalu",
-    UG: "Uganda",
-    UA: "Ukraine",
-    AE: "United Arab Emirates",
-    GB: "United Kingdom",
-    UM: "United States Minor Outlying Islands",
-    US: "United States",
-    UY: "Uruguay",
-    UZ: "Uzbekistan",
-    VU: "Vanuatu",
-    VE: "Venezuela",
-    VN: "Vietnam",
-    VG: "Virgin Islands (British)",
-    VI: "Virgin Islands (U.S.)",
-    WF: "Wallis and Futuna",
-    EH: "Western Sahara",
-    YE: "Yemen",
-    ZM: "Zambia",
-    ZW: "Zimbabwe",
-  });
+  const [modalInfo, setModalInfo] = useState({});
 
   const handlCloseModal = () => {
     setShowModal(false);
+    setModalInfo({});
   };
 
   const regionStyles = {
@@ -331,6 +34,29 @@ const Map = ({ isUS }) => {
     },
   };
 
+  const handleAPIcall = (code) => {
+    // Make a call to the server API
+    const name = regionNames[code];
+    fetch(`/api/${name}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === "success") {
+          console.log("success: ", res.message);
+          setModalInfo(res.data.law)
+        } else {
+          console.error("ERROR", res.message);
+        }
+      })
+      .catch((err) => {
+        console.log("FUCCK: ", err);
+      });
+  };
+
   const handleRegionSelected = (event, code, isSelected, label) => {
     console.log("Region selected:", code, isSelected, label, regionNames[code]);
     let inArr = initialArray.includes(code);
@@ -339,25 +65,7 @@ const Map = ({ isUS }) => {
       initialArray.splice(initialArray.indexOf(code), 1);
       setCode("");
     } else {
-      // Make a call to the server API
-      const name = regionNames[code];
-      fetch(`/api/${name}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.status === "success") {
-            console.log("success: ", data.message);
-          } else {
-            console.error("ERROR" , data.message);
-          }
-        })
-        .catch((err) => {
-          console.log("FUCCK: ", err);
-        });
+      handleAPIcall(code);
       initialArray.push(code);
       setShowModal(true);
       setCode(regionNames[code]);
@@ -370,6 +78,7 @@ const Map = ({ isUS }) => {
         show={showModal}
         handlCloseModal={handlCloseModal}
         name={currCode}
+        modalInfo={modalInfo}
       />
       {/* <div className={`MapToggle ${showModal ? "fade-out" : ""}`}>
         <MapToggle isUSToggle={us} onToggleChange={handleToggle} />
