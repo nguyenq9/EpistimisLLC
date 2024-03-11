2779ba7a81b303505e0d2eba85eea3290357577f
Saunder VanWoerden <saunder.vw@gmail.com>
2024-03-11 12:38:27 -0700

    got rid of regionnamesjson

--- client/src/Components/Map/Map.js
+++ client/src/Components/Map/Map.js
@@ -1,28 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Map.css";
 import { VectorMap } from "@react-jvectormap/core";
 import { usLcc } from "@react-jvectormap/unitedstates";
 import { worldMill } from "@react-jvectormap/world";
-import regionNames from "./regionNames.json";
 import { handleSingleStateRetrieval, handleCompareCall } from "../../js/API";
 import Modal from "../Modal/Modal";
 
 const Map = ({ isUS, showModal, setShowModal }) => {
   const [currCode, setCode] = useState("");
+  const [currRegion, setRegion] = useState("");
   const [modalInfo, setModalInfo] = useState([]);
   const [comparing, setComparing] = useState(false);
   const [selectedRegions, setSelectedRegions] = useState([]);
 
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (currCode !== "") {
+      setRegion(getRegionName(currCode));
+    }
+  }, [currCode]);
+
+  const getRegionName = (code) => {
+    if (mapRef.current) {
+      return mapRef.current.getMapObject().getRegionName(code);
+    }
+  }
+
   // Function to add a region
   const addRegion = (region) => {
-    setSelectedRegions(prevRegions => [...prevRegions, regionNames[region]]);
+    setSelectedRegions(prevRegions => [...prevRegions, getRegionName(region)]);
   }
 
   // Function to remove a region
-  const removeRegion = (region) => {
-    setSelectedRegions(prevRegions => prevRegions.filter(r => r !== region));
-  }
-
+  // const removeRegion = (region) => {
+  //   setSelectedRegions(prevRegions => prevRegions.filter(r => r !== region));
+  // }
 
   const handleCloseModal = () => {
     setCode("")
@@ -49,15 +62,14 @@ const Map = ({ isUS, showModal, setShowModal }) => {
     },
   };
 
-
   const handleRegionSelected = (event, newCode) => {
     setCode(newCode);
     if (comparing) {
       addRegion(currCode)
       addRegion(newCode)
-      handleCompareCall(currCode, newCode, setModalInfo)
+      handleCompareCall(getRegionName(currCode), getRegionName(newCode), setModalInfo)
     } else {
-      handleSingleStateRetrieval(newCode, setModalInfo)
+      handleSingleStateRetrieval(getRegionName(newCode), setModalInfo)
     }
     setShowModal(true);
   };
@@ -73,7 +85,7 @@ const Map = ({ isUS, showModal, setShowModal }) => {
       <Modal
         show={showModal}
         handleCloseModal={handleCloseModal}
-        name={regionNames[currCode]}
+        name={currRegion}
         modalInfo={modalInfo}
         handleCompareClicked={handleCompareClicked}
         comparing={comparing}
@@ -81,6 +93,7 @@ const Map = ({ isUS, showModal, setShowModal }) => {
       />
       <div className="map-container">
         <VectorMap
+          mapRef={mapRef}
           key={isUS ? "usLcc" + comparing : "worldMill" + comparing}
           map={isUS ? usLcc : worldMill}
           style={{
--- client/src/js/API.js
+++ client/src/js/API.js
@@ -1,8 +1,5 @@
-import regionNames from "../Components/Map/regionNames.json"
-
-const handleSingleStateRetrieval = (code, setModalInfo) => {
+const handleSingleStateRetrieval = (name, setModalInfo) => {
   // Make a call to the server API
-  const name = regionNames[code];
   fetch(`/api/${name}`, {
     method: "GET",
     headers: {
@@ -23,10 +20,7 @@ const handleSingleStateRetrieval = (code, setModalInfo) => {
     });
 };
 
-const handleCompareCall = (code1, code2, setModalInfo) => {
-  const name1 = regionNames[code1];
-  const name2 = regionNames[code2];
-
+const handleCompareCall = (name1, name2, setModalInfo) => {
   fetch(`/api/compare/${name1}/${name2}`, {
     method: "GET",
     headers: {
