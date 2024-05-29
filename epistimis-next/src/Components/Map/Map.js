6b6735f292de9867d6af0febd5ddcd4137babb3a
Saunder VanWoerden <saunder.vw@gmail.com>
2024-05-29 11:18:29 -0700

    add glow around map

--- epistimis-next/src/Components/Map/Map.js
+++ epistimis-next/src/Components/Map/Map.js
@@ -151,6 +151,8 @@ const Map = ({ isUS, filterOption, comparing, setComparing, editable, setEditabl
           map={isUS ? usLcc : worldMill}
           style={{
             height: windowHeight * .85,
+            filter: "drop-shadow(0 0 50px rgba(56, 168, 163, 0.5))",
+            // borderColor: getCSSVariableValue("--background")
           }}
           regionsSelectable={true}
           regionsSelectableOne={true}
