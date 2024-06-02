b93b6c9f6f3687e959eb03a6f8a6413ee406caee
melnikd <melnikd@wwu.edu>
2024-06-01 23:31:23 -0700

    some accordion changes

--- epistimis-next/src/Components/Modal/ComprehensiveLaw.js
+++ epistimis-next/src/Components/Modal/ComprehensiveLaw.js
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { Accordion, AccordionItem } from "../Accordion/Accordion";
-
 import './ComprehensiveLaw.css';
 
-export default function comprehensiveLaw({ law }) {
+export default function ComprehensiveLaw({ law }) {
+
+    // Destructure the law object into individual variables
+    const { lawName, acronym, reference, billStatus, inEffect, description, applicability } = law;
+    const {roles, personalDataCategories, processingPurposes ,consent} = applicability;
     return (
         <div className="law">
-            <h2>{law.lawName}</h2>
+            <h2>{lawName}</h2>
             <div className="law-info">
                 <p>Acronym: {law.acronym}</p>
                 <a href={law.reference} target="_blank" className="law-reference">Reference</a>
@@ -14,16 +17,24 @@ export default function comprehensiveLaw({ law }) {
                 <p>Enacted: {law.inEffect}</p>
             </div>
             <p>{law.description}</p>
-            <Accordion>
-                <AccordionItem category="Applicability" info="">
-                    
-                </AccordionItem>
-                <AccordionItem category="Individual Rights" info=""/>
-                <AccordionItem category="Business Obligations" info=""/>
-                <AccordionItem category="Scope" info=""/>
-                <AccordionItem category="Enforcement" info=""/>
-                <AccordionItem category="Thresholds" info=""/>
-            </Accordion>
+            <div className="accordions-container">
+                <Accordion>
+                    <AccordionItem category="Applicability" info="">
+                        <Accordion>
+                            <AccordionItem category="Roles" info={["businesses", "processor", "controller"]} />
+                            <AccordionItem category="Personal Data Categories" info={personalDataCategories} />
+                            <AccordionItem category="Processing Purposes" info={law.applicability.processingPurposes} />
+                            <AccordionItem category="Consent Required" info={law.applicability.consent} />
+                        </Accordion>
+                    </AccordionItem>
+                    <AccordionItem category="Individual Rights" info="Individual Rights information" />
+                    <AccordionItem category="Business Obligations" info="Business Obligations information" />
+                    <AccordionItem category="Scope" info="Scope information" />
+                    <AccordionItem category="Enforcement" info="Enforcement information" />
+                    <AccordionItem category="Thresholds" info="Thresholds information" />
+                </Accordion>
+            </div>
         </div>
     );
 }
+
